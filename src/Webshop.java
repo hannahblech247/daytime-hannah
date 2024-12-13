7bae41ffc35dc3ec897ae5080cf171a3ed3d336a
hannah.blech <hannah.blech@web.de>
2024-12-13 09:24:12 +0100

    update

--- src/Webshop.java
+++ src/Webshop.java
@@ -7,16 +7,49 @@ public class Webshop extends Server {
 
     @Override
     public void processNewConnection(String pClientIP, int pClientPort) {
-        send(pClientIP, pClientPort, "Willkommen im Webshop. In welcher Größe willst du dein Einhornfurzkissen bestellen (S; M; L; XL) und welche Farbe soll es haben?");
+        this.send(pClientIP, pClientPort, "Willkommen im Webshop. In welcher Größe willst du dein Einhornfurzkissen bestellen (S; M; L; XL) und welche Farbe soll es haben?");
     }
 
     @Override
     public void processMessage(String pClientIP, int pClientPort, String pMessage) {
-        String einhornfurzkissen_groesse = pMessage.split(":")[1];
-        String einhornfurzkissen_farbe = pMessage.split(":")[2];
-        if(einhornfurzkissen_groesse == "S" || einhornfurzkissen_groesse == "M" || einhornfurzkissen_groesse == "L" || einhornfurzkissen_groesse == "XL" ){
+        String nachrichtenpart_1 = pMessage.split(":")[0];
+
+        if(nachrichtenpart_1.equals("Einhornfurzkissen")){
+            String einhornfurzkissen_groesse = pMessage.split(":")[1];
+            if(einhornfurzkissen_groesse.equals( "S" )|| einhornfurzkissen_groesse.equals("M")|| einhornfurzkissen_groesse.equals("L") || einhornfurzkissen_groesse.equals("XL")) {
+                send(pClientIP, pClientPort, "Bitte Bestätige deine Bestellung eines Einhornfurzkissens in der Größe" + pMessage.split(":")[1]
+                        + " und in der Farbe" + pMessage.split(":")[2] + " mit ja oder nein");
+            }
+            else{
+                this.send(pClientIP, pClientPort, "Bitte gebe eine Größe aus S;M;L;XL ein!");
+            }
+        }
+        else if(nachrichtenpart_1.equals("BESTÄTIGUNG")){
+            if(pMessage.split(":")[1].equals("ja")){
+                this.send(pClientIP, pClientPort, "Danke für deine Bestellung!");
+            }
+            else if(pMessage.split(":")[1].equals("nein")){
+                this.send(pClientIP,pClientPort, "Du wirst nun abgemeldet und deine Daten werden gelöscht!");
+                this.closeConnection(pClientIP, pClientPort);
+            }
+            else {
+                this.send(pClientIP, pClientPort, "Bitte gebe ja oder nein ein!");
+            }
+
 
         }
+
+        else if(nachrichtenpart_1.equals("ABMELDEN")){
+            this.send(pClientIP,pClientPort,"Du wirst nun abgemeldet und deine Daten gelöscht!");
+            this.closeConnection(pClientIP, pClientPort);
+        }
+
+        else{
+            this.send(pClientIP,pClientPort, "Bitte gebe eine valide Eingabe ein!");
+        }
+
+
+
     }
 
     @Override
